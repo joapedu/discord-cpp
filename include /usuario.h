13748c8a404812baed51eec59d87d3ea6f50e738
João Eduardo <joaoeduardobraga2@gmail.com>
2023-06-27 17:39:58 -0300

    feat: add usuario.h ao projeto

---
+++ include /usuario.h
@@ -0,0 +1,46 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <iostream>
+
+using namespace std;
+
+class Usuario {
+   private:
+    string nome;
+    string email;
+    string senha;
+//será atribuído um id ao usuário
+    int id;
+//controle se o usuário esta logado ou não
+    bool logado = false;
+
+   public:
+//construtor
+    Usuario(int id, string nome, string email, string senha);
+
+//construtor padrão
+    Usuario();
+
+//destrutor
+    ~Usuario();
+
+    int getId();
+    string getNome();
+    string getEmail();
+    string getSenha();
+
+    bool getLogado();
+
+    void setId(int id);
+
+    void setLogado(bool logado);
+
+    void setNome(string nome);
+
+    void setEmail(string email);
+
+    void setSenha(string senha);
+};
+
+#endif
